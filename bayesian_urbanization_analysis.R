ebc966dcedd3e2239b819e65842583900aea5691
Nosa Osawe <osaweemmanuelnosa@gmail.com>
2026-02-16 16:30:03 -0500

    update plots

--- bayesian_urbanization_analysis.R
+++ bayesian_urbanization_analysis.R
@@ -1,12 +1,15 @@
-library(rjags)
-library(coda)
+
+# load 'dataset' object first
+
+# Load libraries ----
 library(tidyverse)
 library(tidybayes)
 library(viridis)
 library(raster)
 library(ggpubr)
+library(coda)
 
-# Load the rjags model fits
+# Load the rjags model fits saved as rds files ----
 
 caterpillarFit = readRDS("caterpillarFit.rds")
 spiderFit      = readRDS("spiderFit.rds")
@@ -18,8 +21,126 @@ grasshopperFit = readRDS("grasshopperFit.rds")
 flyFit         = readRDS("flyFit.rds")
 daddylonglegsFit = readRDS("daddylonglegsFit.rds")
 
+# Arthropod silhouette images ----
+
+
+
+### Arthropod images ----
+catImage = readPNG('images/caterpillar.png')
+antImage = readPNG('images/ant.png')
+beetleImage = readPNG('images/beetle.png')
+spiderImage = readPNG('images/spider.png')
+hopperImage = readPNG('images/leafhopper.png')
+truebugImage = readPNG('images/truebugs.png')
+
+# https://www.phylopic.org/images/7174527d-3060-4c78-ab59-c3ccf76075de/opilio-saxatilis
+daddylonglegImage = image_read('images/daddylongleg.png') %>% 
+  image_convert(type = "truecolor") %>% 
+  as.raster()
+
+# https://www.phylopic.org/images/2bec28d1-3d13-4512-8b4d-00cd0fcef6e4/clogmia-albipunctata
+flyImage = image_read('images/fly.png') %>% 
+  image_convert(type = "truecolor") %>% 
+  as.raster()
+
+# https://www.phylopic.org/images/0de35750-d9ba-472a-b4f2-3c630777fcc3/acrididae
+grasshopperImage = image_read('images/grasshopper.png') %>% 
+  image_convert(type = "truecolor") %>% 
+  as.raster()
+
+
+# simulate hypothesis plot------------------------------------------------------------------
+set.seed(123)
+simulate_lines <- function(beta_dev, beta_int, label) {
+  
+  n <- 500
+  
+  dev <- rnorm(n, 0, 0.4)
+  Latitude <- rnorm(n, 0, 0.5)
+  ObservationMethod <- sample(c("Visual","Beating sheet"), n, replace = TRUE)
+  
+  method_num <- ifelse(ObservationMethod == "Visual", 0, 1)
+  
+  beta0  <- 0.5
+  beta_lat <- 0.5
+  beta_method <- 0.05
+  
+  y <- beta0 +
+    beta_dev*dev +
+    beta_lat*Latitude +
+    beta_int*dev*Latitude +
+    beta_method*method_num +
+    rnorm(n, 0, 0.3)
+  
+  sim_dataset <- data.frame(y, dev, Latitude, ObservationMethod)
+  
+  mod <- lm(y ~ dev * Latitude + ObservationMethod, data = sim_dataset)
+  
+  dev_seq <- seq(min(dev), max(dev), length = 100)
+  
+  lat_vals <- c(-0.7, 0, 0.7)
+  lat_labels <- c("Low","Mid","High")
+  
+  # predict response, hold latitude constant, fix method to visual
+  preds <- lapply(1:3, function(i){
+    predict(mod,
+            newdata = data.frame(dev = dev_seq,
+                                 Latitude = lat_vals[i],
+                                 ObservationMethod = "Visual"))
+  })
+  
+  data.frame(
+    dev = rep(dev_seq, 3),
+    response = unlist(preds),
+    Latitude = factor(rep(lat_labels, each = length(dev_seq))),
+    Scenario = label
+  )
+}
+
+
+plot_data <- rbind(
+  simulate_lines( 1.3, 1.25, "Positive effect, UHI"),
+  simulate_lines( 1.3, 0,    "Positive effect, no UHI"),
+  simulate_lines(-1.3, 1.25, "Negative effect, UHI"),
+  simulate_lines(-1.3, 0,    "Negative effect, no UHI")
+) %>% mutate(Latitude = factor(Latitude, levels= c("High", "Mid", "Low")))
+
+
+hypothesisPlot = ggplot(plot_data, aes(dev, response, color = Latitude)) +
+  geom_line(linewidth = 2) +
+  scale_color_viridis_d() +
+  facet_wrap(~Scenario, ncol = 2) +
+  theme_bw(base_size = 13) +
+  theme(
+    axis.text = element_blank(),
+    axis.ticks = element_blank(),
+    panel.grid = element_blank()
+  ) +labs(y = "Proportion of surey with arthropod",
+          x = "% Urban development")
+
+hypothesisPlot
+
+panel_labels <- data.frame(
+  Scenario = c("Positive effect, UHI",
+               "Positive effect, no UHI",
+               "Negative effect, UHI",
+               "Negative effect, no UHI"),
+  label = c("d", "c", "b", "a"),
+  x = -Inf,
+  y = Inf
+)
+
+hypothesisPlot +
+  geom_text(data = panel_labels,
+            aes(x = x, y = y, label = label),
+            inherit.aes = FALSE,
+            hjust = -0.5,
+            vjust = 1.5,
+            size = 5,
+            fontface = "bold")
 
 ################################################################################
+
 dataset$dev_c = as.numeric(scale(dataset$dev))
 dataset$lat_c  = as.numeric(scale(dataset$Latitude))
 dataset$dev_lat = dataset$dev_c * dataset$lat_c
@@ -70,16 +191,20 @@ caterpillarPred_summary = caterpillarPred_grid_long %>%
     lat_c == 0  ~ "Mid",
     lat_c == 1  ~ "High"
   )) %>%
-  mutate(Latitude = factor(Latitude, levels = c("Low", "Mid", "High")))
+  mutate(Latitude = factor(Latitude, levels = c("Low", "Mid", "High"))) %>% 
+  mutate(dev = dev_c * sd(dataset$dev) + mean(dataset$dev)) # converting back to raw values.
 
 
-CaterpillarPlot = ggplot(caterpillarPred_summary, aes(x = dev_c, y = p_median, color = factor(Latitude))) +
+
+
+
+CaterpillarPlot = ggplot(caterpillarPred_summary, aes(x = dev, y = p_median, color = factor(Latitude))) +
   geom_line(size = 1) +
   geom_ribbon(aes(ymin = p_lower, ymax = p_upper, fill = Latitude), alpha = 0.2, color = NA) +
   scale_color_viridis_d(name = "Latitude") +
   scale_fill_viridis_d(name = "Latitude") +
-  labs(x = "Scaled Urban Development", y = "Predicted Probability of Caterpillar Presence") +
-  annotation_raster(catImage, ymin = .085, ymax = .1, xmin = 0, xmax = 1.5) +
+  labs(x = "% Urban Development", y = "Prop. of surveys with spider presence") +
+  annotation_raster(catImage, ymin = .085, ymax = .1, xmin = 60, xmax = 100) +
   theme_minimal()
 
 
@@ -118,16 +243,17 @@ spiderPred_summary = spiderPred_grid_long %>%
     lat_c == 0  ~ "Mid",
     lat_c == 1  ~ "High"
   )) %>%
-  mutate(Latitude = factor(Latitude, levels = c("Low", "Mid", "High")))
+  mutate(Latitude = factor(Latitude, levels = c("Low", "Mid", "High"))) %>% 
+  mutate(dev = dev_c * sd(dataset$dev) + mean(dataset$dev))
 
 
-spiderPlot = ggplot(spiderPred_summary, aes(x = dev_c, y = p_median, color = factor(Latitude))) +
+spiderPlot = ggplot(spiderPred_summary, aes(x = dev, y = p_median, color = factor(Latitude))) +
   geom_line(size = 1) +
   geom_ribbon(aes(ymin = p_lower, ymax = p_upper, fill = Latitude), alpha = 0.2, color = NA) +
   scale_color_viridis_d(name = "Latitude") +
   scale_fill_viridis_d(name = "Latitude") +
-  labs(x = "Scaled Urban Development", y = "Predicted Probability of spider Presence") +
-  annotation_raster(spiderImage, ymin = .29, ymax = .35, xmin = 0, xmax = 1.5) +
+  labs(x = "% Urban Development", y = "prop. of surveys with spider presence") +
+  annotation_raster(spiderImage, ymin = .29, ymax = .35, xmin = 60, xmax = 100) +
   theme_minimal()
 
 
@@ -166,16 +292,17 @@ beetlePred_summary = beetlePred_grid_long %>%
     lat_c == 0  ~ "Mid",
     lat_c == 1  ~ "High"
   )) %>%
-  mutate(Latitude = factor(Latitude, levels = c("Low", "Mid", "High")))
+  mutate(Latitude = factor(Latitude, levels = c("Low", "Mid", "High"))) %>% 
+  mutate(dev = dev_c * sd(dataset$dev) + mean(dataset$dev))
 
 
-beetlePlot = ggplot(beetlePred_summary, aes(x = dev_c, y = p_median, color = factor(Latitude))) +
+beetlePlot = ggplot(beetlePred_summary, aes(x = dev, y = p_median, color = factor(Latitude))) +
   geom_line(size = 1) +
   geom_ribbon(aes(ymin = p_lower, ymax = p_upper, fill = Latitude), alpha = 0.2, color = NA) +
   scale_color_viridis_d(name = "Latitude") +
   scale_fill_viridis_d(name = "Latitude") +
-  labs(x = "Scaled Urban Development", y = "Predicted Probability of beetle Presence") +
-  annotation_raster(beetleImage, ymin = .29, ymax = .31, xmin = 0, xmax = 1.5)  +
+  labs(x = "% Urban Development", y = "Prop. of surveys with beetle presence") +
+  annotation_raster(beetleImage, ymin = .28, ymax = .30, xmin = 60, xmax = 100)  +
   theme_minimal()
 
 
@@ -216,16 +343,17 @@ truebugPred_summary = truebugPred_grid_long %>%
     lat_c == 0  ~ "Mid",
     lat_c == 1  ~ "High"
   )) %>%
-  mutate(Latitude = factor(Latitude, levels = c("Low", "Mid", "High")))
+  mutate(Latitude = factor(Latitude, levels = c("Low", "Mid", "High"))) %>% 
+  mutate(dev = dev_c * sd(dataset$dev) + mean(dataset$dev))
 
 
-truebugPlot = ggplot(truebugPred_summary, aes(x = dev_c, y = p_median, color = factor(Latitude))) +
+truebugPlot = ggplot(truebugPred_summary, aes(x = dev, y = p_median, color = factor(Latitude))) +
   geom_line(size = 1) +
   geom_ribbon(aes(ymin = p_lower, ymax = p_upper, fill = Latitude), alpha = 0.2, color = NA) +
   scale_color_viridis_d(name = "Latitude") +
   scale_fill_viridis_d(name = "Latitude") +
-  labs(x = "Scaled Urban Development", y = "Predicted Probability of truebug Presence") +
-  annotation_raster(truebugImage, ymin = .1, ymax = .12, xmin = -0.8, xmax = 0.6)  +
+  labs(x = "% Urban Development", y = "Prop. of surveys with truebug presence") +
+  annotation_raster(truebugImage, ymin = .09, ymax = .11, xmin = 0, xmax = 40)  +
   theme_minimal()
 
 
@@ -263,16 +391,17 @@ hopperPred_summary = hopperPred_grid_long %>%
     lat_c == 0  ~ "Mid",
     lat_c == 1  ~ "High"
   )) %>%
-  mutate(Latitude = factor(Latitude, levels = c("Low", "Mid", "High")))
+  mutate(Latitude = factor(Latitude, levels = c("Low", "Mid", "High"))) %>% 
+  mutate(dev = dev_c * sd(dataset$dev) + mean(dataset$dev))
 
 
-hopperPlot = ggplot(hopperPred_summary, aes(x = dev_c, y = p_median, color = factor(Latitude))) +
+hopperPlot = ggplot(hopperPred_summary, aes(x = dev, y = p_median, color = factor(Latitude))) +
   geom_line(size = 1) +
   geom_ribbon(aes(ymin = p_lower, ymax = p_upper, fill = Latitude), alpha = 0.2, color = NA) +
   scale_color_viridis_d(name = "Latitude") +
   scale_fill_viridis_d(name = "Latitude") +
-  labs(x = "Scaled Urban Development", y = "Predicted Probability of True hopper Presence") +
-  annotation_raster(hopperImage, ymin = .07, ymax = .095, xmin = 0, xmax = 1.3)  +
+  labs(x = "% Urban Development", y = "Prop. of surveys with truehopper presence") +
+  annotation_raster(hopperImage, ymin = .07, ymax = .095, xmin = 60, xmax = 100)  +
   theme_minimal()
 
 
@@ -311,16 +440,17 @@ antPred_summary = antPred_grid_long %>%
     lat_c == 0  ~ "Mid",
     lat_c == 1  ~ "High"
   )) %>%
-  mutate(Latitude = factor(Latitude, levels = c("Low", "Mid", "High")))
+  mutate(Latitude = factor(Latitude, levels = c("Low", "Mid", "High"))) %>% 
+  mutate(dev = dev_c * sd(dataset$dev) + mean(dataset$dev))
 
 
-antPlot = ggplot(antPred_summary, aes(x = dev_c, y = p_median, color = factor(Latitude))) +
+antPlot = ggplot(antPred_summary, aes(x = dev, y = p_median, color = factor(Latitude))) +
   geom_line(size = 1) +
   geom_ribbon(aes(ymin = p_lower, ymax = p_upper, fill = Latitude), alpha = 0.2, color = NA) +
   scale_color_viridis_d(name = "Latitude") +
   scale_fill_viridis_d(name = "Latitude") +
-  labs(x = "Scaled Urban Development", y = "Predicted Probability of ant Presence") +
-  annotation_raster(antImage, ymin = .14, ymax = .16, xmin = 0.2, xmax = 1.3)  +
+  labs(x = "% Urban Development", y = "Prop. of surveys with ant presence") +
+  annotation_raster(antImage, ymin = .14, ymax = .16, xmin = 60, xmax = 99)  +
   theme_minimal()
 
 
@@ -360,16 +490,17 @@ grasshopperPred_summary = grasshopperPred_grid_long %>%
     lat_c == 0  ~ "Mid",
     lat_c == 1  ~ "High"
   )) %>%
-  mutate(Latitude = factor(Latitude, levels = c("Low", "Mid", "High")))
+  mutate(Latitude = factor(Latitude, levels = c("Low", "Mid", "High"))) %>% 
+  mutate(dev = dev_c * sd(dataset$dev) + mean(dataset$dev))
 
 
-grasshopperPlot = ggplot(grasshopperPred_summary, aes(x = dev_c, y = p_median, color = factor(Latitude))) +
+grasshopperPlot = ggplot(grasshopperPred_summary, aes(x = dev, y = p_median, color = factor(Latitude))) +
   geom_line(size = 1) +
   geom_ribbon(aes(ymin = p_lower, ymax = p_upper, fill = Latitude), alpha = 0.2, color = NA) +
   scale_color_viridis_d(name = "Latitude") +
   scale_fill_viridis_d(name = "Latitude") +
-  labs(x = "Scaled Urban Development", y = "Predicted Probability of grasshopper Presence") +
-  annotation_raster(grasshopperImage, ymin = .12, ymax = .16, xmin = -0.8, xmax = 0.8)  +
+  labs(x = "% Urban Development", y = "Prop. of surveys with grasshopper presence") +
+  annotation_raster(grasshopperImage, ymin = .12, ymax = .16, xmin = 15, xmax = 60)  +
   theme_minimal()
 
 
@@ -408,16 +539,17 @@ flyPred_summary = flyPred_grid_long %>%
     lat_c == 0  ~ "Mid",
     lat_c == 1  ~ "High"
   )) %>%
-  mutate(Latitude = factor(Latitude, levels = c("Low", "Mid", "High")))
+  mutate(Latitude = factor(Latitude, levels = c("Low", "Mid", "High"))) %>% 
+  mutate(dev = dev_c * sd(dataset$dev) + mean(dataset$dev))
 
 
-flyPlot = ggplot(flyPred_summary, aes(x = dev_c, y = p_median, color = factor(Latitude))) +
+flyPlot = ggplot(flyPred_summary, aes(x = dev, y = p_median, color = factor(Latitude))) +
   geom_line(size = 1) +
   geom_ribbon(aes(ymin = p_lower, ymax = p_upper, fill = Latitude), alpha = 0.2, color = NA) +
   scale_color_viridis_d(name = "Latitude") +
   scale_fill_viridis_d(name = "Latitude") +
-  labs(x = "Scaled Urban Development", y = "Predicted Probability of fly Presence") +
-  annotation_raster(flyImage, ymin = .14, ymax = .17, xmin = -0.8, xmax = 0.6)  +
+  labs(x = "% Urban Development", y = "Prop. of surveys with fly presence") +
+  annotation_raster(flyImage, ymin = .14, ymax = .16, xmin = 20, xmax = 60)  +
   theme_minimal()
 
 
@@ -456,16 +588,17 @@ daddylonglegsPred_summary = daddylonglegsPred_grid_long %>%
     lat_c == 0  ~ "Mid",
     lat_c == 1  ~ "High"
   )) %>%
-  mutate(Latitude = factor(Latitude, levels = c("Low", "Mid", "High")))
+  mutate(Latitude = factor(Latitude, levels = c("Low", "Mid", "High"))) %>% 
+  mutate(dev = dev_c * sd(dataset$dev) + mean(dataset$dev))
 
 
-daddylonglegsPlot = ggplot(daddylonglegsPred_summary, aes(x = dev_c, y = p_median, color = factor(Latitude))) +
+daddylonglegsPlot = ggplot(daddylonglegsPred_summary, aes(x = dev, y = p_median, color = factor(Latitude))) +
   geom_line(size = 1) +
   geom_ribbon(aes(ymin = p_lower, ymax = p_upper, fill = Latitude), alpha = 0.2, color = NA) +
   scale_color_viridis_d(name = "Latitude") +
   scale_fill_viridis_d(name = "Latitude") +
-  labs(x = "Scaled Urban Development", y = "Predicted Probability of daddylonglegs Presence") +
-  annotation_raster(daddylonglegImage, ymin = .04, ymax = .07, xmin = -0.9, xmax = 0.7)  +
+  labs(x = "% Urban Development", y = "Prop. of surveys with harvestman presence") +
+  annotation_raster(daddylonglegImage, ymin = .04, ymax = .06, xmin = 60, xmax = 90)  +
   theme_minimal()
 
 
@@ -483,32 +616,7 @@ ggarrange(CaterpillarPlot, spiderPlot, beetlePlot, truebugPlot, hopperPlot, antP
 
 ##################################################################################
 
-mean_dev = mean(dataset$dev)
-sd_dev  = sd(dataset$dev)
-
-mean_lat = mean(dataset$Latitude)
-sd_lat = sd(dataset$Latitude)
-post = as.matrix(caterpillarFit)
-beta_dev_orig = post[, "beta_dev"] / sd_dev
-beta_lat_orig = post[, "beta_lat"] / sd_lat
-beta_int_orig = post[, "beta_int"] / (sd_dev * sd_lat)
-
-beta_0_orig =
-  post[, "beta_0"] -
-  post[, "beta_dev"] * mean_dev / sd_dev -
-  post[, "beta_lat"] * mean_lat / sd_lat +
-  post[, "beta_int"] * mean_dev * mean_lat / (sd_dev * sd_lat)
-
-beta_method_orig = post[, "beta_method"]
-median(beta_dev_orig)
-quantile(beta_dev_orig, c(0.025, 0.975))
-
-
-OR_dev <- exp(beta_dev_orig)
-median(OR_dev)
-quantile(OR_dev, c(0.025, 0.975))
-
-
+ 
 
 
 
